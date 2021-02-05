7d1c92f866a3ba970fbf6ba24c5b5fccc6204aa1
Denys Rocha <denys.rocha@getrak.com.br>
2021-02-05 16:37:36 -0300

    First commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import './assets/scss/main.scss'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+function importMobileSass() {
+  if (screen.width <= 591.98) {
+    require('./assets/scss/mobile.scss')
+  }
+}
+
+importMobileSass()
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
